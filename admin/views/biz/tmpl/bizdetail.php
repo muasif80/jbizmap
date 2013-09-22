e840f1e58cacdb306aac951698ed40a4bd682f20
muasif80 <muasif80@gmail.com>
2013-09-23 00:26:05 +0500

    Map is added to the bizdetail layout for biz view
    The map is added to the bizdetail layout so that the business is shown
    on the map.

--- admin/views/biz/tmpl/bizdetail.php
+++ admin/views/biz/tmpl/bizdetail.php
@@ -2,6 +2,48 @@
 	$biz = $this->item;
 ?>
 
+<script type="text/javascript">
+var markerLat = '<?php echo $biz->bizloclat; ?>';
+var markerLng = '<?php echo $biz->bizloclng; ?>';
+
+function initialize() {
+
+  var pos = new google.maps.LatLng(markerLat, markerLng);
+	  
+  var mapOptions = {
+    zoom: 14,
+    
+    //center: new google.maps.LatLng(-34.397, 150.644),
+    //center: new google.maps.LatLng(markerLat, markerLng),
+  	center: pos,  
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+  
+  //bounds.extend(pos);
+  marker = new google.maps.Marker({
+      position: pos,
+      map: map
+  });
+  
+}
+
+function loadScript() {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyAMHb77EGZrdSHwgK5gKdgo4C10L-Rjb_Y&sensor=true&callback=initialize";
+  document.body.appendChild(script);
+
+//  initialize();
+}
+
+window.onload = loadScript;
+
+</script>
+
+
+
 <h2><?php echo $biz->bizname; ?></h2>
 
 <table class="table table-striped" style="width: 600px;">
@@ -35,3 +77,8 @@
 	</tr>
 	
 </table>
+
+<div class="map-wrapper" style="width: 100%; height: 400px; border: solid thick #CCDDCC;">
+	<div id="map-canvas" style="width: 100%; height: 100%;">
+	</div>
+</div>
